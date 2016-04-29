2bc58373c68519be369de36f21d75b472f1c7fb7
Staffan Garhammar <staffan.garhammar@viaplay.com>
2016-04-29 19:18:28 +0200

    added production config

---
+++ webpack.config.production.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = [
+  {
+    entry: [
+      path.resolve(__dirname, 'public/lysa.js')
+    ],
+    output: {
+      path: path.resolve(__dirname, 'public/dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract(
+            "style",
+            "css?sourceMap!sass?sourceMap"
+          )},
+        {
+          test: /.js$/,
+          loaders: ['babel']
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].css'),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  }
+];
